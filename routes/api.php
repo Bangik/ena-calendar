cb1d19fa383931759be7b6ea1dde0f5d7bc6e663
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2022-06-28 13:20:26 +0700

    update route categories

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,11 @@ Route::fallback(function(){
 });
 
 Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
 Route::get('/events', [EventController::class, 'index']);
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
